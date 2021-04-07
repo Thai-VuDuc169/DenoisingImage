5a73c0adaa0cfac230debfb2dc0a8f60bed5133f
Thai-VuDuc169 <vuducthaibka62@gmail.com>
2021-04-07 23:54:50 +0700

    add EvaluateImages.py

--- EvaluateImages.py
+++ EvaluateImages.py
@@ -1,11 +1,25 @@
-import numpy as np
-import copy
-x = np.array([1, 2, 3])
-y = x
-z = copy.deepcopy(x)
-y[0] = 10
-print (z)
-print (y)
-print(x)
-print (x[0] == y[0])
-print (x[0] == z[0])
+from skimage.metrics import peak_signal_noise_ratio, structural_similarity
+
+
+class Evaluator:
+    def __init__(self, input_img):
+        self.base_image = input_img
+    
+    # Compute the peak signal to noise ratio (PSNR) for an image.
+    # return dB
+    def calcPSNR(self, image_test):
+        return peak_signal_noise_ratio(image_true= self.base_image, 
+                                        image_test= image_test,
+                                        data_range= 255)
+
+    # Compute the mean structural similarity index between two images.
+    # return 0->1 value
+    def calcSSIM(self, image_test):
+        return structural_similarity(im1= self.base_image,
+                                        im2= image_test,
+                                        data_range= 255)
+
+    def evalImages(self, image_test):
+        PSNR_value = self.calcPSNR(image_test)
+        SSIM_value = self.calcSSIM(image_test)
+        return PSNR_value, SSIM_value
--- Main.py
+++ Main.py
@@ -1,26 +1,36 @@
 from BasicFilters import *
-from cv2 import cv2 as cv
+import cv2 as cv
 import numpy as np
 import cv2 as cv
 import os
 import random
 random.seed(10)
 import matplotlib.pyplot as plt
+import pathlib
 
-path = r"E:\20202\Image Processing\DenoisingImage\Original_data\Degraded"
-random_file = random.choice(os.listdir(path))
-img = cv.imread(path + "\\"  + random_file , 0)
+current_folder_path = str(pathlib.Path(__file__).parent.absolute())
+print("Hello {}!, the current folder path is '{}' in this system"
+        .format(os.getlogin(), current_folder_path ))
+
+random_file = random.choice(os.listdir(current_folder_path))
+img = cv.imread(current_folder_path + "\\"  + random_file , 0)
 
 Filter.setInputImage(img)
 mean_filter = MeanFilter()
 median_filter = MedianFilter()
 gaussian_blur = GaussianBlur()
 laplacian_filter = LaplacianFilter()
+opening_filter = OpeningFilter()
 
 filter_list = [mean_filter, median_filter, gaussian_blur, laplacian_filter]
+
+
+plt.subplot((int(len(dir_folder)/2) + 2), 2, 1)
+plt.title("INPUT IMAGE")
+plt.imshow(self.input_img)
 for index, e in enumerate(filter_list, 1):
     plt.subplot()
-    plt.imshow( i.filterImage())
-    plt.
+    plt.imshow(i.filterImage())
+    # plt.
     
 plt.show()
